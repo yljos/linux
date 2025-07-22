867af025415a47a157b6ccb5a011dad38f714f1b
yljos <yljos@outlook.com>
2025-07-23 07:29:11 +0800

    fix

--- wayland_dotfiles/.config/windows.sh
+++ wayland_dotfiles/.config/windows.sh
@@ -2,7 +2,7 @@
 # 直接连接指定IP，全部后台运行
 (
     notify-send "连接中" "启动 RDP..." && play ~/.config/dunst/connecting.mp3 >/dev/null 2>&1
-    xfreerdp3 /v:192.168.31.121 /u:huai /p:110 /sound /dynamic-resolution /cert:ignore >/dev/null 2>&1 &
+    xfreerdp3 /v:192.168.31.156 /u:huai /p:110 /sound /dynamic-resolution /cert:ignore >/dev/null 2>&1 &
     RDP_PID=$!
     sleep 5
     if kill -0 "$RDP_PID" 2>/dev/null; then
