ad85639f503e4693336b12d6fdad30a962c25886
yljos <yljos@outlook.com>
2025-08-02 15:17:46 +0800

    format

--- nas/ufw.sh
+++ nas/ufw.sh
@@ -1,5 +1,4 @@
-# Description: This script is used to configure the ufw firewall on the host machine.
-#!/bin/bash
+#!/usr/bin/sh
 
 # 重置防火墙规则
 sudo ufw reset
@@ -23,8 +22,6 @@ sudo ufw allow proto tcp to any port 80    # HTTP
 sudo ufw allow proto tcp to any port 443   # HTTPS
 
 # NFSv4 端口
-# sudo ufw allow proto tcp from 192.168.31.25 to any port 111    # rpcbind TCP
-# sudo ufw allow proto udp from 192.168.31.25 to any port 111    # rpcbind UDP
 sudo ufw allow proto tcp from 192.168.31.25 to any port 2049   # NFSv4 主端口
 
 # Podman 容器 TCP 端口
