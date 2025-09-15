38885140130acbcc21fe0888805b2541c62b4ed6
yljos <yljos@outlook.com>
2025-09-15 15:19:13 +0800

    165

--- arch/huai/.p10k.zsh
+++ arch/huai/.p10k.zsh
@@ -182,7 +182,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=129
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=165
   # Custom icon.
   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='Arch'
 
