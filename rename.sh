46012f35e1bfd0bf6204c98250db2b50648b6158
yljos <yljos@outlook.com>
2025-09-16 19:34:36 +0800

    clean

--- rename.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-find . -depth -name '* *' | while IFS= read -r file; do
-	mv -- "$file" "${file// /_}"
-done
-find . -depth -name "*'*" | while IFS= read -r file; do
-	mv -- "$file" "${file//\'/_}"
-done
