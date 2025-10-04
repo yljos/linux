f0c266f92a30762977311a4a7932646031f65808
yljos <yljos@outlook.com>
2025-10-04 15:48:17 +0800

    lock.sh

--- Arch/huai_dwl/.config/shutdown.sh
+++ Arch/huai_dwl/.config/shutdown.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/dash
+#!/usr/bin/bash
+
+# --- 加载脚本锁库 ---
+source "$HOME/.config/script_lock.sh"
+# --- 检查脚本锁 ---
+acquire_script_lock || exit 0
 
 SHUTDOWN_FILE="http://192.168.31.21/shutdown"
 INTERVAL=300 # 循环间隔秒
