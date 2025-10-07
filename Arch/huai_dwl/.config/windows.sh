7a5473bfad83126633dce862d13e865a948c26df
yljos <yljos@outlook.com>
2025-10-07 12:03:08 +0800

    test

--- Arch/huai_dwl/.config/windows.sh
+++ Arch/huai_dwl/.config/windows.sh
@@ -31,7 +31,7 @@ done
 	connect_to_host() {
 		notify-send "连接中" "启动 RDP..." && play ~/.config/dunst/connecting.mp3 >/dev/null 2>&1
 		#xfreerdp3 /v:"$TARGET_IP" /u:huai /p:"$PASSWORD" /w:1902 /h:1032 /sound /cert:ignore >/dev/null 2>&1 &
-		xfreerdp3 /v:"$TARGET_IP" /u:huai /p:"$PASSWORD" /dynamic-resolution /f /sound /cert:ignore >/dev/null 2>&1 &
+		xfreerdp3 /v:"$TARGET_IP" /u:huai /p:"$PASSWORD" /dynamic-resolution /f /smart-sizing /desktop-composition /sound /cert:ignore >/dev/null 2>&1 &
 		RDP_PID=$!
 		sleep 10
 		if kill -0 "$RDP_PID" 2>/dev/null; then
