3dda83fe85651cf31f0e6ef674efb16eb8a27a2d
yljos <yljos@outlook.com>
2025-11-26 09:58:38 +0800

    fix

--- Arch/huai/.config/dwl/ime.sh
+++ Arch/huai/.config/dwl/ime.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-#
-# 包装脚本：切换 fcitx5 输入法，并发送信号通知 dwl_status 刷新
 
-# 1. 切换输入法
+# 1. 切换 fcitx5 输入法状态
 fcitx5-remote -t
 
-# 2. 使用 Python 脚本通知状态栏更新输入法显示
-python3 "$HOME/.config/dwl/notify_ime.py" 2>/dev/null
+# 2. 发送信号给 dwl_status.sh 立即刷新
+
+pkill -RTMIN+3 -f dwl_status.sh
