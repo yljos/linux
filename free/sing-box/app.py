825be2af464c3ffc8e82b233d50c90a2b49aa3a7
yljos <yljos@outlook.com>
2025-12-15 20:09:59 +0800

    fix

--- free/sing-box/app.py
+++ free/sing-box/app.py
@@ -225,4 +225,4 @@ def parse_node_url(url: str) -> Dict[str, Any]:
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
