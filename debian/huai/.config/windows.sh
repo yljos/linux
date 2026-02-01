db4a65452d123523bc7429254ada8ac4f8d3318e
yljos <yljos@outlook.com>
2026-02-01 11:33:12 +0000

    fix

--- debian/huai/.config/windows.sh
+++ debian/huai/.config/windows.sh
@@ -7,7 +7,7 @@ TARGET_IP="10.0.0.15"
 
 # 使用 --localnet 扫描全网，并匹配目标 MAC 地址
 # 2>/dev/null 屏蔽杂乱的输出
-if sudo arp-scan --localnet 2>/dev/null | grep -q "$MAC_ADDRESS"; then
+if sudo arp-scan -localnet 2>/dev/null | grep -q "$MAC_ADDRESS"; then
     notify-send "Windows" "主机已在线，直接连接"
 else
     # 不在线，发送唤醒包
