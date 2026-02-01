6bb67c984645551592087d5dc71777ed8bc574a1
yljos <yljos@outlook.com>
2026-02-01 10:35:28 +0000

    win

--- debian/huai/.config/windows.sh
+++ debian/huai/.config/windows.sh
@@ -4,10 +4,8 @@
 MAC_ADDRESS="00:23:24:67:DF:14"
 BROADCAST_IP="10.0.0.255"
 
-# 发送唤醒指令
-if wakeonlan -i "$BROADCAST_IP" "$MAC_ADDRESS" >/dev/null 2>&1; then
-    notify-send "WOL" "唤醒魔术包已发送"
-else
-    notify-send "错误" "唤醒失败，请检查 wakeonlan 是否安装"
-    exit 1
-fi
+wakeonlan -i "$BROADCAST_IP" "$MAC_ADDRESS" >/dev/null 2>&1; then
+notify-send "WOL" "唤醒魔术包已发送"
+
+sleep 15
+xfreerdp3 /v:10.0.0.15 /u:huai /p:123 /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
