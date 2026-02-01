4530783faa814c99e2db8cfd3f1671282192af4d
yljos <yljos@outlook.com>
2026-02-01 10:59:25 +0000

    fix win

--- debian/huai/.config/windows.sh
+++ debian/huai/.config/windows.sh
@@ -3,9 +3,14 @@
 # 配置
 MAC_ADDRESS="00:23:24:67:DF:14"
 BROADCAST_IP="10.0.0.255"
+TARGET_IP="10.0.0.15"
 
-wakeonlan -i "$BROADCAST_IP" "$MAC_ADDRESS" >/dev/null 2>&1; then
-notify-send "WOL" "唤醒魔术包已发送"
+# 修复：增加了 if 和 fi
+if wakeonlan -i "$BROADCAST_IP" "$MAC_ADDRESS" >/dev/null 2>&1; then
+    notify-send "Windows" "唤醒魔术包已发送，等待启动..."
+fi
 
 sleep 15
-xfreerdp3 /v:10.0.0.15 /u:huai /p:123 /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+
+# 启动 RDP
+xfreerdp3 /v:"$TARGET_IP" /u:huai /p:123 /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
