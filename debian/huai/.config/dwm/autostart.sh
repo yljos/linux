70b77a69a50a76b2a10d03fd14d239cff4bd179c
yljos <yljos@outlook.com>
2026-02-01 08:09:56 +0000

    fix

---
+++ debian/huai/.config/dwm/autostart.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+play ~/.config/dunst/xp.wav &
+
+if ! pgrep -f "firefox" &>/dev/null; then
+	sleep 3
+	firefox-esr &
+fi
+
+if ! pgrep -x "Telegram" &>/dev/null; then
+	Telegram &
+fi
+
+
+if [ -f ~/.config/shutdown.sh ]; then
+	/bin/bash ~/.config/shutdown.sh &
+fi
+
+if command -v numlockx &>/dev/null; then
+	numlockx &
+fi
+
+if ! pgrep -x "picom" &>/dev/null; then
+	picom -b
+fi
+
+if ! pgrep -x "fcitx5" &>/dev/null; then
+	fcitx5 -d
+fi
+
+if ! pgrep -x "dunst" &>/dev/null; then
+	dunst &
+fi
+
+sleep 3
+
+if [ -f ~/.config/dwm/dwm_status.sh ] && ! pgrep -f "bash.*dwm_status.sh" &>/dev/null; then
+	/bin/bash ~/.config/dwm/dwm_status.sh &
+fi
