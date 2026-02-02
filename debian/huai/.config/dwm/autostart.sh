22e4c6dc8ebae6e725d738504b31192b5ce84573
yljos <yljos@outlook.com>
2026-02-02 07:51:35 +0000

    fix debian dwm autostart

--- debian/huai/.config/dwm/autostart.sh
+++ debian/huai/.config/dwm/autostart.sh
@@ -1,39 +1,17 @@
 #!/usr/bin/env bash
 
+sleep 0.5
+[[ -f /home/huai/.config/dwm/dwm_status.sh ]] && /bin/bash /home/huai/.config/dwm/dwm_status.sh &
 mpv --no-video /home/huai/.config/dunst/xp.wav &
 
-if ! pgrep -f "firefox" &>/dev/null; then
-	sleep 3
-	firefox-esr &
-fi
+command -v numlockx &>/dev/null && numlockx &
+! pgrep -x "picom" &>/dev/null && picom -b
+! pgrep -x "fcitx5" &>/dev/null && fcitx5 -d
+! pgrep -x "dunst" &>/dev/null && dunst &
 
-if ! pgrep -x "Telegram" &>/dev/null; then
-	Telegram &
-fi
+[[ -f /home/huai/.config/shutdown.sh ]] && /bin/bash /home/huai/.config/shutdown.sh &
+[[ -f /home/huai/.config/wallpaperchange.sh ]] && /bin/bash /home/huai/.config/wallpaperchange.sh
 
-
-if [ -f /home/huai/.config/shutdown.sh ]; then
-	/bin/bash /home/huai/.config/shutdown.sh &
-fi
-
-if command -v numlockx &>/dev/null; then
-	numlockx &
-fi
-
-if ! pgrep -x "picom" &>/dev/null; then
-	picom -b
-fi
-
-if ! pgrep -x "fcitx5" &>/dev/null; then
-	fcitx5 -d
-fi
-
-if ! pgrep -x "dunst" &>/dev/null; then
-	dunst &
-fi
-
-sleep 3
-
-if [ -f /home/huai/.config/dwm/dwm_status.sh ] && ! pgrep -f "bash.*dwm_status.sh" &>/dev/null; then
-	/bin/bash /home/huai/.config/dwm/dwm_status.sh &
-fi
+sleep 1
+! pgrep -f "firefox" &>/dev/null && firefox-esr &
+! pgrep -x "Telegram" &>/dev/null && Telegram &
