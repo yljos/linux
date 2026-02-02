4638fe50d64786fd6f322ac05e623a7e38d7c1bf
yljos <yljos@outlook.com>
2026-02-02 07:54:49 +0000

    fix

--- debian/huai/.config/dwm/autostart.sh
+++ debian/huai/.config/dwm/autostart.sh
@@ -4,14 +4,15 @@ sleep 0.5
 [[ -f /home/huai/.config/dwm/dwm_status.sh ]] && /bin/bash /home/huai/.config/dwm/dwm_status.sh &
 mpv --no-video /home/huai/.config/dunst/xp.wav &
 
-command -v numlockx &>/dev/null && numlockx &
-! pgrep -x "picom" &>/dev/null && picom -b
-! pgrep -x "fcitx5" &>/dev/null && fcitx5 -d
-! pgrep -x "dunst" &>/dev/null && dunst &
-
 [[ -f /home/huai/.config/shutdown.sh ]] && /bin/bash /home/huai/.config/shutdown.sh &
 [[ -f /home/huai/.config/wallpaperchange.sh ]] && /bin/bash /home/huai/.config/wallpaperchange.sh
 
 sleep 1
 ! pgrep -f "firefox" &>/dev/null && firefox-esr &
 ! pgrep -x "Telegram" &>/dev/null && Telegram &
+
+command -v numlockx &>/dev/null && numlockx &
+! pgrep -x "picom" &>/dev/null && picom -b
+! pgrep -x "fcitx5" &>/dev/null && fcitx5 -d
+sleep 10
+! pgrep -x "dunst" &>/dev/null && dunst &
