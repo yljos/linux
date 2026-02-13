abc1343e1b77585646cc2150bf4e1437974a744e
yljos <yljos@outlook.com>
2026-02-13 09:11:04 +0000

    remove feh

--- debian/huai/.config/wallpaperchange.sh
+++ debian/huai/.config/wallpaperchange.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-feh --bg-fill --randomize /home/huai/Pictures
+#feh --bg-fill --randomize /home/huai/Pictures
 # feh --bg-fill /home/huai/Pictures/1.jpg
+xwallpaper --maximize "$(find /home/huai/Pictures -type f | shuf -n 1)"
