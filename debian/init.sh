bc68ecb63026e2fe6b84d70e7d13a091f8cbd162
yljos <yljos@outlook.com>
2026-03-20 02:14:07 +0000

    fix init

--- debian/init.sh
+++ debian/init.sh
@@ -1,54 +1,35 @@
-#!/bin/bash
+# ---------------------------------------------------------
+# [1] Directories
+# ---------------------------------------------------------
+mkdir -p /home/huai/.ssh
+mkdir -p /home/huai/.gnupg
+mkdir -p /home/huai/.config/systemd/user
 
 # ---------------------------------------------------------
-# [1] System Update & Base Tools
+# [2] Data Directory
 # ---------------------------------------------------------
-sudo apt update
-sudo apt install -y sudo git curl vim nfs-common
-
-# Create /data with specific permissions
-# This is required for your mount-data function
 sudo mkdir -p /data
 sudo chown huai:huai /data
 sudo chmod 755 /data
 
-# Locale and Fonts
+# ---------------------------------------------------------
+# [3] Package Installation
+# ---------------------------------------------------------
+sudo apt update
+sudo apt install -y git curl vim nfs-common build-essential \
+    libx11-dev libxinerama-dev libxft-dev xserver-xorg xinit \
+    freerdp2-x11 freerdp3-x11 scdaemon pcscd \
+    fonts-noto-cjk fonts-noto-color-emoji libnotify-bin \
+    pipewire wireplumber
+
+# ---------------------------------------------------------
+# [4] System Configuration
+# ---------------------------------------------------------
 sudo dpkg-reconfigure locales
-sudo apt install -y fonts-noto-cjk fonts-noto-color-emoji
-
-# ---------------------------------------------------------
-# [2] User Space Directory Structure
-# ---------------------------------------------------------
-mkdir -p ~/.ssh ~/.gnupg ~/.config/systemd/user
-chmod 700 ~/.gnupg
-
-# ---------------------------------------------------------
-# [3] DWM & X11 Build Dependencies
-# ---------------------------------------------------------
-sudo apt install -y \
-    build-essential \
-    libx11-dev libxinerama-dev libxft-dev \
-    xserver-xorg xinit
-
-# ---------------------------------------------------------
-# [4] Desktop Services & Audio
-# ---------------------------------------------------------
-# Audio: Pure Native PipeWire
-sudo apt install -y pipewire wireplumber libnotify-bin
-
-# Enable PipeWire services for current user
 systemctl --user --now enable pipewire wireplumber
 
-# GPG Smartcard & Secret Management (for your key() function)
-sudo apt install -y scdaemon pcscd
-
 # ---------------------------------------------------------
-# [5] Remote Desktop & Cleanup
+# [5] Network Service Swap
 # ---------------------------------------------------------
-# Install FreeRDP3
-sudo apt install -y freerdp3-x11
-
-# Final Cleanup
-sudo apt autoremove -y
-
-
+sudo systemctl disable --now networking
+sudo systemctl enable --now systemd-networkd
