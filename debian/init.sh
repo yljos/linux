ecec3cfcb4e2abf9ec021349e6f315f341ed59f6
yljos <yljos@outlook.com>
2026-03-20 02:24:58 +0000

    improve init

--- debian/init.sh
+++ debian/init.sh
@@ -1,9 +1,13 @@
-# ---------------------------------------------------------
+#!/bin/bash
+
 # [1] Directories
 # ---------------------------------------------------------
 mkdir -p /home/huai/.ssh
 mkdir -p /home/huai/.gnupg
 mkdir -p /home/huai/.config/systemd/user
+
+# Ensure GPG directory is secure
+chmod 700 /home/huai/.gnupg
 
 # ---------------------------------------------------------
 # [2] Data Directory
@@ -16,7 +20,7 @@ sudo chmod 755 /data
 # [3] Package Installation
 # ---------------------------------------------------------
 sudo apt update
-sudo apt install -y git curl vim nfs-common build-essential \
+sudo apt install -y locales git curl vim nfs-common build-essential \
     libx11-dev libxinerama-dev libxft-dev xserver-xorg xinit \
     freerdp2-x11 freerdp3-x11 scdaemon pcscd \
     fonts-noto-cjk fonts-noto-color-emoji libnotify-bin \
@@ -25,7 +29,12 @@ sudo apt install -y git curl vim nfs-common build-essential \
 # ---------------------------------------------------------
 # [4] System Configuration
 # ---------------------------------------------------------
-sudo dpkg-reconfigure locales
+# Overwrite locale.gen directly for en_US.UTF-8
+echo "en_US.UTF-8 UTF-8" | sudo tee /etc/locale.gen > /dev/null
+sudo locale-gen
+sudo update-locale LANG=en_US.UTF-8
+
+# Enable audio services
 systemctl --user --now enable pipewire wireplumber
 
 # ---------------------------------------------------------
