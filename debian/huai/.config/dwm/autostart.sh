ee283c1a296590670a8569a06b6fc33ff2112c33
bite-os <yljos@outlook.com>
2026-03-25 01:33:05 +0000

    fix

--- debian/huai/.config/dwm/autostart.sh
+++ debian/huai/.config/dwm/autostart.sh
@@ -11,5 +11,5 @@ command -v numlockx &>/dev/null && numlockx &
 [[ -f /home/huai/.config/wallpaperchange.sh ]] && /bin/bash /home/huai/.config/wallpaperchange.sh
 
 sleep 1
-! pgrep -f "librewolf" &>/dev/null && librewof &
+! pgrep -f "librewolf" &>/dev/null && librewolf &
 # ! pgrep -x "Telegram" &>/dev/null && Telegram &
