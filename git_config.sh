96acab95cc9673898ec899bdbb5d099c97ad165a
yljos <yljos@ihuai.top>
2026-05-07 06:23:20 +0000

    fix

--- git_config.sh
+++ git_config.sh
@@ -2,7 +2,7 @@
 git config --global core.editor "vim"
 git config --global user.signingkey CABB8B2144528A69
 git config --global commit.gpgsign true
-git config --global user.name "bite-os"
-git config --global user.email "bite-os@biteos.org"
+git config --global user.name "yljos"
+git config --global user.email "yljos@ihuai.top"
 
 curl -s "http://10.0.0.21/key/20260429_pub.asc" | gpg --import >/dev/null 2>&1
