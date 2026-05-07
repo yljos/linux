5654ab4841bc70590840e1ddf6a32892cc5d4106
yljos <yljos@ihuai.top>
2026-05-07 06:45:25 +0000

    fix

--- debian/init.sh
+++ debian/init.sh
@@ -51,16 +51,7 @@ sudo systemctl disable --now networking
 sudo systemctl enable --now systemd-networkd
 
 # ---------------------------------------------------------
-# [6] Git Configuration
-# ---------------------------------------------------------
-git config --global core.editor "vim"
-git config --global user.signingkey CABB8B2144528A69
-git config --global commit.gpgsign true
-git config --global user.name "bite-os"
-git config --global user.email "bite-os@biteos.org"
-
-# ---------------------------------------------------------
-# [7] SSH & GPG Keys
+# [6] SSH & GPG Keys
 # ---------------------------------------------------------
 
-curl -sL "http://10.0.0.21/key/20260429_pub.asc" | gpg --import >/dev/null 2>&1
+curl -sL "http://10.0.0.21/key/yljos_pub.asc" | gpg --import >/dev/null 2>&1
