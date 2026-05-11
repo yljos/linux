a161587d8798688156dcd4922cb5669c86e2fbec
yljos <yljos@ihuai.top>
2026-05-11 06:28:39 +0000

    fix

--- nas/init.sh
+++ nas/init.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-rsync -r huai/ /home/huai/
+rsync -r root/ /root/
 
 if [[ -d "etc" ]]; then
 	rsync -r etc/ /etc/
 fi
-# Fix directory permissions
-chown huai:huai -R /home/huai/
 
 # Fix file permissions
-find /home/huai/.ssh  -type d -exec chmod 700 {} +
-find /home/huai/.ssh  -type f -exec chmod 600 {} +
+find /root/.ssh  -type d -exec chmod 700 {} +
+find /root/.ssh  -type f -exec chmod 600 {} +
 
