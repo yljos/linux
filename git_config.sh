95ffe5be9a1e38badce37bc6f00d0a0d93eaa73f
yljos <yljos@ihuai.top>
2026-05-12 08:15:34 +0000

    fix

--- git_config.sh
+++
@@ -1,1 +0,0 @@
-/bin/bash: line 1: shfmt: command not found
--- void/init.sh
+++ void/init.sh
@@ -2,12 +2,22 @@
 
 rsync -r huai/ /home/huai/
 
-if [[ -d "etc" ]]; then
-	rsync -r etc/ /etc/
-fi
 # Fix directory permissions
 chown huai:huai -R /home/huai/
 
 # Fix file permissions
 find /home/huai/.ssh /home/huai/.gnupg -type d -exec chmod 700 {} +
 find /home/huai/.ssh /home/huai/.gnupg -type f -exec chmod 600 {} +
+
+# git
+git config --global core.editor "vim"
+git config --global user.signingkey CABB8B2144528A69
+git config --global commit.gpgsign true
+git config --global user.name "yljos"
+git config --global user.email "yljos@ihuai.top"
+
+curl -s "http://10.0.0.21/key/20260429_pub.asc" | gpg --import >/dev/null 2>&1
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
