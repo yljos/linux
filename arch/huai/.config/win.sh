306e26df7338512cac4672bd34a95f619892097b
yljos <yljos@ihuai.top>
2026-05-23 07:43:53 +0000

    termux bashrc

--- arch/huai/.config/win.sh
+++ arch/huai/.config/win.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
-
 IP="10.0.0.15"
 PASS="123"
-
 # Execute the entire process in a background subshell to prevent blocking
 xfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
