be2ada811c3f96cd0aa03aa0ea5d02e276d9c73b
yljos <yljos@ihuai.top>
2026-05-23 07:30:02 +0000

    fix_win.sh

--- arch/huai/.config/win.sh
+++ arch/huai/.config/win.sh
@@ -6,4 +6,4 @@ IFACE="enp0s31f6"
 PASS="123"
 
 # Execute the entire process in a background subshell to prevent blocking
-xfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1
+xfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
