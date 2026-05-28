51f52eb7ea47e09b973d27e09c3e427c5373bc84
yljos <yljos@ihuai.top>
2026-05-28 06:33:47 +0000

    add win git config sh

---
+++ win/git.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git config --global core.editor "vim"
+git config --global user.signingkey CABB8B2144528A69
+git config --global commit.gpgsign true
+git config --global user.name "yljos"
+git config --global user.email "yljos@ihuai.top"
+git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
