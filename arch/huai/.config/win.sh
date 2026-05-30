dbb912438ea64e803a0e508619946b090f7df654
yljos <yljos@ihuai.top>
2026-05-30 02:53:27 +0000

    win fix

--- arch/huai/.config/win.sh
+++ arch/huai/.config/win.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
+
+# Require password as the first argument
+if [ -z "$1" ]; then
+    echo "Usage: win <password>"
+    exit 1
+fi
+
 IP="10.0.0.15"
-PASS="123"
+PASS="$1"
+
 # Execute the entire process in a background subshell to prevent blocking
 xfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
