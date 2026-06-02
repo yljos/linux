9562c4fa690627d91f13398cc621cbb868d90714
yljos <yljos@ihuai.top>
2026-06-02 12:12:00 +0000

    firefox 1.50 js

--- firefox/1.50/user.js
+++ firefox/1.50/user.js
@@ -1,1 +1,27 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+/** MOZILLA UI ***/
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.profiles.enabled", true);
+
+// CPU & UI rendering limits
+user_pref("image.animation_mode", "none");
+user_pref("toolkit.cosmeticAnimations.enabled", false);
+user_pref("reader.parse-on-load.enabled", false);
+user_pref("browser.urlbar.maxRichResults", 3);
+
+// Enforce H.264 hardware decoding 
+user_pref("media.av1.enabled", false);
+user_pref("media.mediasource.vp9.enabled", false);
+user_pref("media.vp9.enabled", false);
+user_pref("media.mediasource.webm.enabled", false);
+user_pref("media.windows-media-foundation.enabled", true);
