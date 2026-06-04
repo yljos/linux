0b006f2aa8efac3005e98828138de0ce5bea7d58
yljos <yljos@ihuai.top>
2026-06-04 10:53:01 +0000

    win.sh dwm dwl

--- arch/huai/.config/win.sh
+++ arch/huai/.config/win.sh
@@ -9,6 +9,9 @@ fi
 IP="10.0.0.15"
 PASS="$1"
 
-# Execute the entire process in a background subshell to prevent blocking
-xfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
-wlfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+# Execute xfreerdp3 if XWayland/X11 is available, otherwise fallback to wlfreerdp3
+if [ -n "$DISPLAY" ]; then
+    xfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+else
+    wlfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+fi
