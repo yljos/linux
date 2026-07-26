6c1e1ef53608a940cbb163bd5fdb9ccaac730f7f
yljos <git@sakuraos.com>
2026-07-26 01:19:46 +0000

    fix

--- arch/git.sh
+++ arch/git.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 git config --global core.editor "vim"
-git config --global user.signingkey CABB8B2144528A69
+git config --global user.signingkey 4005DCF7C751DCD3257FBD13D74AA1C746C47427
 git config --global commit.gpgsign true
 git config --global user.name "yljos"
-git config --global user.email "yljos@ihuai.top"
+git config --global user.email "git@sakuraos.com"
 
 systemctl --user daemon-reload
 systemctl --user enable --now pipewire wireplumber ssh-agent >/dev/null 2>&1
--- win/git.sh
+++ win/git.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 git config --global core.editor "vim"
-git config --global user.signingkey CABB8B2144528A69
+git config --global user.signingkey 4005DCF7C751DCD3257FBD13D74AA1C746C47427
 git config --global commit.gpgsign true
 git config --global user.name "yljos"
-git config --global user.email "yljos@ihuai.top"
+git config --global user.email "git@sakuraos.com"
 git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
 
 Get-Service ssh-agent | Set-Service -StartupType Automatic
