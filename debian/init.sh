299f232a7523e21fbdc81e8e0be60dbcafed9d16
yljos <git@sakuraos.com>
2026-08-08 05:08:22 +0000

    fix

--- debian/init.sh
+++ debian/init.sh
@@ -9,7 +9,7 @@ sudo apt install --no-install-recommends \
     build-essential libx11-dev libxft-dev libxinerama-dev \
     git vim ca-certificates rsync nfs-common \
     fonts-noto-cjk fonts-hack \
-    freerdp2-x11 x11-xserver-utils
+    freerdp2-x11 x11-xserver-utils arp-scan 
 
 # Update font cache
 fc-cache -fv
