bff1971c928ec32f6ff18be1ef3b3e0940176dc7
yljos <git@sakuraos.com>
2026-08-08 06:49:12 +0000

    fix

--- debian/config.sh
+++ debian/config.sh
@@ -17,6 +17,7 @@ find /home/huai/.ssh /home/huai/.gnupg -type f -exec chmod 600 {} + 2>/dev/null
 systemctl daemon-reload
 systemctl enable --now shutdown >/dev/null 2>&1
 # systemctl enable --now pcscd.socket
+# systemctl --user enable --now pipewire wireplumber pipewire-pulse
 
 tee /etc/krb5.conf </dev/null
 
--- debian/init.sh
+++ debian/init.sh
@@ -8,8 +8,9 @@ sudo apt install --no-install-recommends \
     xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xinit \
     build-essential libx11-dev libxft-dev libxinerama-dev \
     git vim ca-certificates rsync nfs-common \
-    fonts-noto-cjk fonts-hack \
-    freerdp2-x11 x11-xserver-utils arp-scan mpv
+    fonts-noto-cjk fonts-hack gpg \
+    freerdp2-x11 x11-xserver-utils arp-scan mpv \
+    pipewire wireplumber pipewire-pulse pipewire-alsa
 
 # Update font cache
 fc-cache -fv
