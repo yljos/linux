2738f11e1262650486f064faf399e94d1a7c95dc
yljos <git@sakuraos.com>
2026-08-08 04:58:42 +0000

    fix

--- debian/config.sh
+++ debian/config.sh
@@ -14,9 +14,9 @@ chown huai:huai -R /home/huai/
 find /home/huai/.ssh /home/huai/.gnupg -type d -exec chmod 700 {} + 2>/dev/null
 find /home/huai/.ssh /home/huai/.gnupg -type f -exec chmod 600 {} + 2>/dev/null
 
-# systemctl daemon-reload
-# systemctl enable --now shutdown >/dev/null 2>&1
-# systemctl enable --now pcscd.socket
+systemctl daemon-reload
+systemctl enable --now shutdown >/dev/null 2>&1
+systemctl enable --now pcscd.socket
 
 tee /etc/krb5.conf </dev/null
 
