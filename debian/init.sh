77fa48b9027e53644c42328e0e4ad6da6168f481
yljos <git@sakuraos.com>
2026-08-08 04:56:16 +0000

    fix

--- debian/init.sh
+++ debian/init.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-# Update package list and install minimal Xorg, build tools, dependencies, and vim
+# Update package list
 sudo apt update
-sudo apt install --no-install-recommends xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xinit build-essential libx11-dev libxft-dev libxinerama-dev git vim ca-certificates
 
-# Install open-source Noto CJK fonts
-sudo apt install fonts-noto-cjk fonts-hack
-
-# freerdp(debian12)
-sudo apt install freerdp2-x11 rsync
-
-sudo apt install x11-xserver-utils
+# Install minimal X11, build tools, fonts, and utilities
+sudo apt install --no-install-recommends \
+    xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xinit \
+    build-essential libx11-dev libxft-dev libxinerama-dev \
+    git vim ca-certificates rsync nfs-common \
+    fonts-noto-cjk fonts-hack \
+    freerdp2-x11 x11-xserver-utils
 
 # Update font cache
 fc-cache -fv
-
-
 
