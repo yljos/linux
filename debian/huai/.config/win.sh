584ffa3714f56a34810d31eddbf7e42126572458
yljos <git@sakuraos.com>
2026-08-08 04:54:17 +0000

    fix

---
+++ debian/huai/.config/win.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# Define variables
+MAC="00:23:24:67:DF:14"
+IP="10.0.0.15"
+
+# Check if the host is online using arp-scan, wake and wait if offline
+if ! sudo arp-scan "$IP" | grep -qi "$MAC"; then
+	wakeonlan -i 10.0.0.255 "$MAC" >/dev/null 2>&1
+
+	# Wait until the host is up, max 10 loops
+	count=0
+	while ! sudo arp-scan "$IP" | grep -qi "$MAC"; do
+		[ "$count" -ge 10 ] && exit 1
+		sleep 3
+		((count++))
+	done
+fi
+
+# Read password from file
+read -r PASS <"$PASS_FILE"
+
+# Prioritize Wayland, fallback to X11 directly
+if [ -n "$WAYLAND_DISPLAY" ]; then
+	wlfreerdp3 /v:"$IP" /u:huai /p:"$PASS" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+else
+	xfreerdp /v:"$IP" /u:huai /p:"123" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+fi
