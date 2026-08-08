59296bd202f551a48a55b5c643236229de4c5470
yljos <git@sakuraos.com>
2026-08-08 07:27:52 +0000

    test

--- debian/huai/.config/win.sh
+++ debian/huai/.config/win.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/bash
 
-# Define variables
-MAC="00:23:24:67:DF:14"
-IP="10.0.0.15"
+# Wrap all commands in a subshell and run it in the background
+(
+    MAC="00:23:24:67:DF:14"
+    IP="10.0.0.15"
 
-# Check if the host is online using arp-scan, redirect stderr to hide warnings
-if ! sudo arp-scan "$IP" 2>/dev/null | grep -qi "$MAC"; then
-    wakeonlan -i 10.0.0.255 "$MAC" >/dev/null 2>&1
+    if ! sudo arp-scan "$IP" 2>/dev/null | grep -qi "$MAC"; then
+        wakeonlan -i 10.0.0.255 "$MAC" >/dev/null 2>&1
 
-    # Wait until the host is up, max 10 loops
-    count=0
-    while ! sudo arp-scan "$IP" 2>/dev/null | grep -qi "$MAC"; do
-        [ "$count" -ge 10 ] && exit 1
-        sleep 3
-        ((count++))
-    done
-fi
-xfreerdp /v:"$IP" /u:huai /p:"123" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+        count=0
+        while ! sudo arp-scan "$IP" 2>/dev/null | grep -qi "$MAC"; do
+            [ "$count" -ge 10 ] && exit 1
+            sleep 3
+            ((count++))
+        done
+    fi
+    
+    # Run xfreerdp detached from the subshell
+    nohup xfreerdp /v:"$IP" /u:huai /p:"123" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+) >/dev/null 2>&1 &
+
+# Detach the subshell from the terminal
+disown
