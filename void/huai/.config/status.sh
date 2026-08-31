9f5e6236462924b1e60005c062f8dac75b4530d8
yljos <git@sakuraos.com>
2026-08-31 23:39:56 +0000

    fix

--- void/huai/.config/status.sh
+++ void/huai/.config/status.sh
@@ -39,7 +39,7 @@ update_cpu() {
 	PREV_CPU=$curr_cpu
 	PREV_IDLE=$curr_idle
 	# Format CPU usage to 3 digits
-	CPU_STATUS="$(printf "%3d%%" "$usage")"
+	CPU_STATUS="$(printf "%03d%%" "$usage")"
 }
 
 update_mem() {
@@ -75,8 +75,8 @@ update_net() {
 	local tx_kb=$((TX_DIFF / 1024))
 	
 	# Format network rates to 7 characters (6 digits + K)
-	local rx_str=$(printf "%6dK" "$rx_kb")
-	local tx_str=$(printf "%6dK" "$tx_kb")
+	local rx_str=$(printf "%06dK" "$rx_kb")
+	local tx_str=$(printf "%06dK" "$tx_kb")
 	NET_STATUS_STR="${ICON_NET_DOWN}${rx_str} ${ICON_NET_UP}${tx_str}"
 }
 
