2f9c634798345e4eae5bb827f80a4928bde08e09
yljos <git@sakuraos.com>
2026-08-31 23:27:40 +0000

    fix

--- void/huai/.config/win.sh
+++ void/huai/.config/win.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-nohup xfreerdp /v:"10.0.0.15" /u:huai /p:"123" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
+nohup xfreerdp3 /v:"10.0.0.15" /u:huai /p:"123" /cert:ignore /sound /w:1916 /h:1056 >/dev/null 2>&1 &
