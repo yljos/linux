fd9749fac97ed0e5b9d32b1fea830fe9067061e6
yljos <git@sakuraos.com>
2026-09-22 17:04:51 +0800

    test

--- convert/app_clash.py
+++ convert/app_clash.py
@@ -57,7 +57,7 @@ SHARED_EXCLUDE_KEYWORDS = [
 ]
 
 # Set to any file name in the directory (e.g., "wukong", "shaseng")
-ACTUAL_SOURCE = "mitce"
+ACTUAL_SOURCE = "westdata"
 
 app = Flask(__name__)
 
